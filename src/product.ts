f1c07ae8ad5318e36e8106cd96146eb2511d4a43
Kevincheng0804 <11130473@me.mcu.edu.tw>
2024-04-24 00:28:00 +0800

    feat(ts):變更待產品建構式

---
+++ src/product.ts
@@ -0,0 +1,19 @@
+export class Product {
+  id: number;
+  name: string;
+  company: string;
+  price: number;
+  isShow: boolean;
+  createDate: Date;
+  modifyDate: Date | undefined;
+
+  constructor(id: number, name: string, company: string, price: number, isShow: boolean, createDate: Date, modifyDate: Date | undefined) {
+    this.id = id;
+    this.name = name;
+    this.company = company;
+    this.price = price;
+    this.isShow = isShow;
+    this.createDate = createDate;
+    this.modifyDate = modifyDate;
+  }
+}
