d14a469a294b0b85a2b8fb108bb65d64f311414b
Kevincheng0804 <11130473@me.mcu.edu.tw>
2024-04-24 00:38:12 +0800

    feat(ts):利用陣列紀錄多筆產品

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Product } from "./product";
 
 const product = new Product(1, "圖像 Angular 開發入門 第二版", "博碩文化", 500, true, new Date(2024, 4, 23), undefined);
-console.log(product);
+
+const product2 = new Product(1, "金魚都能懂得 CSS 必學屬性", "博碩文化", 500, true, new Date(2024, 4, 23), undefined);
+
+const products = [product, product2];
+console.log(products[0]);
+console.log(products[1]);
 
 //npx ts-node ./src/index.ts
