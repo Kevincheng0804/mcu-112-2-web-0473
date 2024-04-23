9d348626c53613a2a0fca0d1789e81bb9fb3b84b
Kevincheng0804 <11130473@me.mcu.edu.tw>
2024-04-23 22:36:46 +0800

    feat(ts):利用聯集變數紀錄產品修改日期

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,7 @@ console.log("Is Show =", isShow);
 
 let createDate = new Date();
 console.log("Create Date", createDate);
+
+let modifyDate: Date | undefined = undefined;
+console.log("Modify Date", modifyDate);
 //npx ts-node ./src/index.ts
